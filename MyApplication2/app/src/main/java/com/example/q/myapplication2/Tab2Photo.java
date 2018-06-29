cfeea7e7c37313fd7f74d407be3d7156b780753a
alphaJu <chongyeju@naver.com>
2018-06-29 20:25:01 +0900

    MyApplication2

---
+++ MyApplication2/app/src/main/java/com/example/q/myapplication2/Tab2Photo.java
@@ -0,0 +1,17 @@
+package com.example.q.myapplication2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Photo extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2photo, container, false);
+
+        return rootView;
+    }
+}
